ffe858cc92e27d937442fd83d55ff9885ea79b83
Michael LaCroix <michael@lacroixdesign.net>
2014-05-05 17:29:37 -0500

    add Clicky analytics

--- source/javascripts/features/pjax.js
+++ source/javascripts/features/pjax.js
@@ -1,12 +1,21 @@
 (function() {
-  var google = window[window.GoogleAnalyticsObject];
+  $(document).on('pjax:complete', function() {
 
-  if (google) {
-    $(document).on('pjax:complete', function() {
+    var google = window[window.GoogleAnalyticsObject];
+    if (google) {
       google('set', 'location', window.location.href);
       google('send', 'pageview');
-    });
-  }
+    }
+
+    if (!!window.clicky) {
+      window.clicky.log(
+        document.location.pathname + document.location.search,
+        document.title,
+        'pageview'
+      );
+    }
+
+  });
 
   $(document).pjax('a', '#content', {fragment: '#content'});
 })();
