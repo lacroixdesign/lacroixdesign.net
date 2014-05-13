46a47f620309324cca1e384e99f17787a48c17d1
Michael LaCroix <michael@lacroixdesign.net>
2014-05-13 16:58:57 -0500

    add the production version of Cloud.typography

--- config.rb
+++ config.rb
@@ -106,13 +106,15 @@ set :fonts_dir,  'fonts'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  activate :minify_css, :inline => true
+  activate :minify_css,
+            :ignore => [%r{fonts}],
+            :inline => true
 
   # Minify Javascript on build
   activate :minify_javascript, :inline => true
 
   # Enable cache buster
-  activate :asset_hash
+  activate :asset_hash, :ignore => [%r{^fonts/165355}]
 
   activate :minify_html
   activate :gzip
